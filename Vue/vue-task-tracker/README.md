92b02c317b95f32ed7beb3a613810bf713f36016
zh-D <1641245614@qq.com>
2021-08-09 23:17:26 +0800

    vue-task-tracker

---
+++ Vue/vue-task-tracker/README.md
@@ -0,0 +1,3 @@
+# vue-task-tracker
+
+vue and vue-router
---
+++ Vue/vue-task-tracker/src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- v-on就是@ <button v-on:click="onClick()" :style="{ background: color }" class="btn">{{ text }}</button> -->
+  <button @click="onClick()" :style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    onClick() {
+      this.$emit("btn-click");
+    },
+  },
+};
+</script>
