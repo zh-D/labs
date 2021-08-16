22bf1dcd9bf76ecf063627f3fce5843dc04fe8e4
zh-D <1641245614@qq.com>
2021-08-16 15:53:51 +0800

    try https

---
+++ nodejs/node-ssl-server/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const https = require('https')
+const path = require('path')
+const fs = require('fs')
+
+const app = express()
+
+app.use('/', (req, res, next) => {
+    res.send('Hello from SSL server')
+})
+
+const sslServer = https.createServer({
+    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
+}, app)
+
+
+sslServer.listen(3443, () => console.log('Secure server run on port 3443'))
