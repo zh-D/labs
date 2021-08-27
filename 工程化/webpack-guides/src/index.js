dad1ee201cb181132bc85260865960194bb7c101
zh-D <1641245614@qq.com>
2021-08-27 20:53:36 +0800

    Caching

--- 工程化/webpack-guides/src/index.js
+++ 工程化/webpack-guides/src/index.js
@@ -1,15 +1,14 @@
+import _ from 'lodash';
+// import Print from './print';
 
-async function getComponent() {
+function component() {
     const element = document.createElement('div');
-    const { default: _ } = await import('lodash');
 
-
+    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+    // element.onclick = Print.bind(null, 'Hello webpack!');
 
     return element;
 }
 
-getComponent().then((component) => {
-    document.body.appendChild(component);
-});
+document.body.appendChild(component());
--- 工程化/webpack-guides/webpack.config.js
+++ 工程化/webpack-guides/webpack.config.js
@@ -12,13 +12,26 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Development',
+            title: 'Caching',
         }),
     ],
     output: {
-        filename: '[name].bundle.js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
         publicPath: '/',
+    },
+    optimization: {
+        moduleIds: 'deterministic',
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                },
+            },
+        },
     },
 };
