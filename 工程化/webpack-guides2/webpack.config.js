20614144828605e49c62f34a8f678476540e535a
zh-D <1641245614@qq.com>
2021-08-28 14:07:58 +0800

    Environment Variables

--- 工程化/webpack-guides2/webpack.config.js
+++ 工程化/webpack-guides2/webpack.config.js
@@ -19,3 +19,19 @@ module.exports = {
         },
     },
 };
+
+// const path = require('path');
+
+// module.exports = (env) => {
+//   // Use env.<YOUR VARIABLE> here:
+//   console.log('Goal: ', env.goal); // 'local'
+//   console.log('Production: ', env.production); // true
+
+//   return {
+//     entry: './src/index.js',
+//     output: {
+//       filename: 'bundle.js',
+//       path: path.resolve(__dirname, 'dist'),
+//     },
+//   };
+// };
